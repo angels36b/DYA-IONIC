cf69fe61504e2cccbf9f479cacda7e2123aa3d56
Angel David <angel.36b@gmail.com>
2018-08-31 22:27:22 -0500

    rootpage para el Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,42 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import {DenunciaPage} from '../pages/denuncia/denuncia';
+import {EscudoPage} from '../pages/escudo/escudo';
+import {HistorialPage} from '../pages/historial/historial';
+import { PoliciaPage } from '../pages/policia/policia';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
-
+  @ViewChild('NAV') nav: Nav;
+  //public rootPage:any = HomePage;
+ public rootPage : any;
+  public pages: Array <{ titulo: string, component:any, icon:string}>
+  
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+   
+    this.rootPage = HomePage;
+    this.pages = [
+    {titulo: "Denuncie",           component: DenunciaPage, icon:"megaphone"},
+    {titulo: "Historial",          component: HistorialPage, icon:"analitycs"},
+    {titulo: "Ver cuadrantes",     component: PoliciaPage, icon:"information-circle"},
+    {titulo: "Escudo comunitario", component: EscudoPage, icon:"home"}
+  ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  
+  }
+  goToPage(page){
+    this.nav.setRoot(page);
   }
 }
 
---
+++ src/pages/escudo/escudo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EscudoPage } from './escudo';
+
+@NgModule({
+  declarations: [
+    EscudoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EscudoPage),
+  ],
+})
+export class EscudoPageModule {}
